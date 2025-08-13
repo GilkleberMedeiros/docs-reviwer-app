88b636cd8635f24148139ca875166b9210eb73a6
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-08-13 16:30:41 -0300

    fix: using pathlib instead of problematic os.path and more options.

--- app/confs.py
+++ app/confs.py
@@ -2,12 +2,18 @@
 Static settings app file. Put your static at runtime settings here.
 """
 
-import os
+from pathlib import Path
 
-PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+__PROJECT_DIR_OBJ_PATH = Path(__file__).parent.parent.absolute()
+PROJECT_DIR = str(__PROJECT_DIR_OBJ_PATH)
+
+# App confs
+ACCEPT_DOCS_FORMATS = [".pdf", ".csv", ".txt", ".docx"]
 
 # LLM model confs
 LLM_MODEL = "gemini-2.5-flash"
+LLM_API_KEY_NAME = "GOOGLE_AISTUDIO_API_KEY"
 
 # Embeddings confs
 EMBEDDINGS_API_KEY_NAME = "GOOGLE_AISTUDIO_API_KEY"
@@ -15,4 +21,4 @@ EMBEDDINGS_MODEL = "models/gemini-embedding-001"
 
 # VectorDB confs
 COLLECTION_NAME = "docs"
-PERSIST_DIR = os.path.join(PROJECT_DIR, "/vectordata/")
+PERSIST_DIR = str(__PROJECT_DIR_OBJ_PATH.joinpath("/vectordata/").absolute())
