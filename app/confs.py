1af921b89f1ea430fd0d8b515a1a02d08add7499
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-08-13 10:14:03 -0300

    feat: adding llm model interface/singleton

--- app/confs.py
+++ app/confs.py
@@ -1,15 +1,18 @@
 """
 Static settings app file. Put your static at runtime settings here.
 """
+
 import os
 
 PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
+# LLM model confs
+LLM_MODEL = "gemini-2.5-flash"
 
 # Embeddings confs
-EMBEDDINGS_API_KEY_NAME="GOOGLE_AISTUDIO_API_KEY"
-EMBEDDINGS_MODEL="models/gemini-embedding-001"
+EMBEDDINGS_API_KEY_NAME = "GOOGLE_AISTUDIO_API_KEY"
+EMBEDDINGS_MODEL = "models/gemini-embedding-001"
 
 # VectorDB confs
-COLLECTION_NAME="docs"
-PERSIST_DIR=os.path.join(PROJECT_DIR, "/vectordata/")
+COLLECTION_NAME = "docs"
+PERSIST_DIR = os.path.join(PROJECT_DIR, "/vectordata/")
--- app/llm.py
+++ app/llm.py
@@ -1,3 +1,44 @@
 """
 Llm model handler, guarantees one instance/connection across application and pre-confs.
 """
+
+from langchain_google_genai.llms import ChatGoogleGenerativeAI
+from abc import ABC
+
+from app.confs import LLM_MODEL
+
+
+class __AbstractLlmModel(ABC):
+    __instance: ChatGoogleGenerativeAI
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.__instance = self._init_instance()
+
+    def get_instance(self) -> ChatGoogleGenerativeAI:
+        raise NotImplementedError()
+
+    def _init_instance(self) -> ChatGoogleGenerativeAI:
+        raise NotImplementedError()
+
+
+class LlmModel(__AbstractLlmModel):
+    __instance: ChatGoogleGenerativeAI
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_instance(self) -> ChatGoogleGenerativeAI:
+        if self.__instance:
+            return self.__instance
+
+        return self._init_instance()
+
+    def _init_instance(self) -> ChatGoogleGenerativeAI:
+        llm = ChatGoogleGenerativeAI(model=LLM_MODEL)
+
+        return llm
+
+
+def get_instance() -> ChatGoogleGenerativeAI:
+    return LlmModel().get_instance()
