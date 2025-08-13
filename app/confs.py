823413b7f0a92b1a020f9e9cf9a2a7ecce8d1b64
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-08-12 21:02:22 -0300

    feat: adding first confs to confs.py

--- app/confs.py
+++ app/confs.py
@@ -1,3 +1,15 @@
 """
 Static settings app file. Put your static at runtime settings here.
 """
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+# Embeddings confs
+EMBEDDINGS_API_KEY_NAME="GOOGLE_AISTUDIO_API_KEY"
+EMBEDDINGS_MODEL="models/gemini-embedding-001"
+
+# VectorDB confs
+COLLECTION_NAME="docs"
+PERSIST_DIR=os.path.join(PROJECT_DIR, "/vectordata/")
