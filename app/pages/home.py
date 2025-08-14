c5953b4f5a9ff407a7c25a6b4d65cef8d6ab605f
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-08-13 22:33:55 -0300

    fix: deleting files from vectorstore after single query with docs.

--- app/pages/home.py
+++ app/pages/home.py
@@ -7,8 +7,7 @@ from services.vectorstore import get_instance as get_vs_instance
 from services.llm import get_instance as get_llm_instance
 from utils import load_documents as ld
 
-
-is_processing: bool = False
+st.session_state["is_req_processing"] = False
 st.container(
     horizontal_alignment="center",
     vertical_alignment="center",
@@ -22,47 +21,53 @@ chat_input = st.chat_input(
 
 if chat_input:
     status = st.status(
-        "Processing request...", state="complete" if is_processing else "running"
+        "Processing request...",
+        state="complete" if st.session_state["is_req_processing"] else "running",
     )
     files = chat_input.files
     query = chat_input.text
     status.write("Got chat inputs...")
 
-    documents = ld.load_documents(files)  # type: ignore
-    status.write("Loaded files into documents...")
-
-    splitted: list[list[Document]] = []
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=700,
-        chunk_overlap=200,
-    )
-
-    for doc in documents:
-        splitted.append(splitter.split_documents(doc))
-    status.write("Splitted documents...")
-
     vectorstore = get_vs_instance()
-    llm = get_llm_instance()
+    doc_ids: list[list[str]] | None = None
+    results: list[Document] | None = None
+    if files:
+        documents = ld.load_documents(files)  # type: ignore
+        status.write("Loaded files into documents...")
 
-    doc_ids = [vectorstore.add_documents(doc) for doc in splitted]
+        splitted: list[list[Document]] = []
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=700,
+            chunk_overlap=200,
+        )
 
-    results: list[Document] = vectorstore.similarity_search(query, k=8)
-    status.write("Got relevant documents...")
+        for doc in documents:
+            splitted.append(splitter.split_documents(doc))
+        status.write("Splitted documents...")
+
+        doc_ids = [vectorstore.add_documents(doc) for doc in splitted]
+
+        results = vectorstore.similarity_search(query, k=8)
+        status.write("Got relevant documents...")
 
     # mount prompt
     status.write("Assembling prompt...")
     docs_str = ""
-    for doc in results:
-        docs_str += doc.page_content + "\n"
+    if results:
+        for doc in results:
+            docs_str += doc.page_content + "\n"
 
     prompt = f"context: \n{docs_str}\n\nquestion: {query}"
 
     status.write("Invoked llm...")
+    llm = get_llm_instance()
     res = llm.invoke(prompt)
 
-    if res.content: # type: ignore
+    print("Res: ", res.text())
+    if res.content:  # type: ignore
         with st.chat_message("assistant"):
-            st.write(res.content) # type: ignore
+            st.write(res.text())  # type: ignore
 
-    [vectorstore.delete(docs_id) for docs_id in doc_ids]
-    is_processing = True
+    if doc_ids:
+        [vectorstore.delete(docs_id) for docs_id in doc_ids]
+    st.session_state["is_req_processing"] = True
